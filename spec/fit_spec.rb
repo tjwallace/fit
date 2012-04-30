afffe42c24c451c89e332415cb837ad0b62271b6
Jeff Wallace <jeff@tjwallace.ca>
2012-04-29 21:09:15 -0700

    remove bogus spec

--- spec/fit_spec.rb
+++ spec/fit_spec.rb
@@ -7,12 +7,4 @@ describe Fit do
     it { should be_a(String) }
     it { should match(/\d{1,2}\.\d{1,2}\.\d{1,2}/) }
   end
-
-  describe ".load_file" do
-    let(:path) { example_file('activity.fit') }
-
-    it "should load a fit file given a path" do
-      described_class.load_file(path).should be_a(Fit::File)
-    end
-  end
 end
