de08ab115123e6245690434483316a10a63772d6
Guillaume Virlet <github@virlet.org>
2015-01-04 17:50:42 +0100

    enhance header to support 14 bytes long headers (based on FIT protocol Rev 1.7)

--- spec/file/header_spec.rb
+++ spec/file/header_spec.rb
@@ -12,4 +12,17 @@ describe Fit::File::Header do
     its(:data_size) { should == 36069 }
     its(:data_type) { should == ".FIT" }
   end
+
+  context "given a sample header file of 14 bytes length" do
+    subject do
+      described_class.read example_file('file/header_14b.fit')
+    end
+
+    its(:header_size) { should == 14 }
+    its(:protocol_version) { should == 16 }
+    its(:profile_version) { should == 411 }
+    its(:data_size) { should == 325 }
+    its(:data_type) { should == ".FIT" }
+    its(:crc) { should == 17101 }
+  end
 end
