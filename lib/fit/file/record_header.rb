c0dd987113afbeeeb4403f31a9104e3ae895414b
Karl-Petter Åkesson <karl-petter@yelloworb.com>
2017-03-02 17:04:21 +0100

    Added developer_data flag that was recently added to the FIT protocol

--- lib/fit/file/record_header.rb
+++ lib/fit/file/record_header.rb
@@ -1,12 +1,14 @@
 module Fit
   class File
     class RecordHeader < BinData::Record
+      # described in section 4.1 Record Header Byte of the FIT Protocol documentation 
       hide :reserved_bits
 
       bit1 :header_type
 
       bit1 :message_type, :onlyif => :normal?
-      bit2 :reserved_bits, :onlyif => :normal?
+      bit1 :message_type_specific, :initial_value => 0, :onlyif => :normal?
+      bit1 :reserved_bits, :onlyif => :normal?
 
       choice :local_message_type, :selection => :header_type do
         bit4 0
@@ -21,6 +23,10 @@ module Fit
 
       def compressed_timestamp?
         header_type == 1
+      end
+      
+      def developer_data?
+        message_type_specific == 1
       end
     end
   end
