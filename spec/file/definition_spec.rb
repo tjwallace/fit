64661361e466ddbfbf85d925d7fd7f291ffbffab
Jeff Wallace <jeff@tjwallace.ca>
2021-09-17 16:30:32 -0700

    Fix specs

--- spec/file/definition_spec.rb
+++ spec/file/definition_spec.rb
@@ -10,16 +10,16 @@ describe Fit::File::Definition do
       its(:architecture) { should == 0 }
       its(:global_message_number) { should == 0 }
       its(:field_count) { should == 6 }
-      it { expect(subject.fields.size).to eq(subject.field_count) }
+      it { expect(subject.fit_fields.size).to eq(subject.field_count) }
 
       its(:record_type) { should == :definition }
       it 'returns the real type for fields' do
-        expect(subject.fields[0].real_type).to be == :uint32z
-        expect(subject.fields[1].real_type).to be == :date_time
-        expect(subject.fields[2].real_type).to be == :manufacturer
-        expect(subject.fields[3].real_type).to be == :uint16 # product
-        expect(subject.fields[4].real_type).to be == :uint16 # number
-        expect(subject.fields[5].real_type).to be == :file
+        expect(subject.fit_fields[0].real_type).to be == :uint32z
+        expect(subject.fit_fields[1].real_type).to be == :date_time
+        expect(subject.fit_fields[2].real_type).to be == :manufacturer
+        expect(subject.fit_fields[3].real_type).to be == :uint16 # product
+        expect(subject.fit_fields[4].real_type).to be == :uint16 # number
+        expect(subject.fit_fields[5].real_type).to be == :file
       end
     end
   end
